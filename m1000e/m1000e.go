e9939df8c3e18607d7f5fecf8fb3f1739be04a1b
Joel Rebello <joel.rebello@booking.com>
2018-04-21 20:06:20 +0200

    Update Login() to retrieve SessionToken, required to POST values.

--- m1000e/m1000e.go
+++ m1000e/m1000e.go
@@ -93,7 +93,38 @@ func (m *M1000e) Login() (err error) {
 		return err
 	}
 
+	// retrieve session token to set config params.
+	token, err := m.getSessionToken()
+	if err != nil {
+		return err
+	}
+	m.SessionToken = token
+
 	return err
+}
+
+// retrieves ST2 which is required to submit form data
+func (m *M1000e) getSessionToken() (token string, err error) {
+
+	u := fmt.Sprintf("https://%s/cgi-bin/webcgi/general", m.ip)
+	resp, err := m.client.Get(u)
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return token, err
+	}
+
+	//<input xmlns="" type="hidden" value="2a17b6d37baa526b75e06243d34763da" name="ST2" id="ST2" />
+	reToken := "<input.*value=\\\"(\\w+)\\\" name=\"ST2\""
+	re := regexp.MustCompile(reToken)
+	match := re.FindSubmatch(data)
+	if len(match) == 0 {
+		return token, errors.ErrUnableToGetSessionToken
+	}
+
+	return string(match[1]), err
 }
 
 func (m *M1000e) loadHwData() (err error) {
