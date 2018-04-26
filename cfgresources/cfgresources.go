356bd62c707f8f508183a510651f344368990c13
Joel Rebello <joel.rebello@booking.com>
2018-04-26 16:31:56 +0200

    cfgresources: define structs for config resource types

---
+++ cfgresources/cfgresources.go
@@ -0,0 +1,46 @@
+package cfgresources
+
+//if a resource is added/updated here it needs to be updated in bmc butler as well,
+//bmc butlers resources need to be the same as bmclib resources,
+//this is until we figure how we want these two packages to depend on each other.
+type User struct {
+	Name     string `yaml:"name"`
+	Password string `yaml:"password"`
+	Role     string `yaml:"role"`
+	Enable   bool   `yaml:"enable,omitempty"`
+}
+
+type Syslog struct {
+	Server string `yaml:"server"`
+	Port   int    `yaml:"port",omitempty`
+	Enable bool   `yaml:"enable",omitempty`
+}
+
+type Ldap struct {
+	Server  string `yaml:"server"`
+	Port    int    `yaml:"port"`
+	Enable  bool   `yaml:"enable"`
+	Role    string `yaml:"role"`
+	BaseDn  string `yaml:"baseDn"`
+	GroupDn string `yaml:"groupDn"`
+}
+
+type Network struct {
+	Hostname    string `yaml:"hostname"`
+	DNSFromDHCP bool   `yaml:"dnsfromdhcp"`
+}
+
+type Ntp struct {
+	Server1  string `yaml:"server1"`
+	Server2  string `yaml:"server2"`
+	Server3  string `yaml:"server3"`
+	Timezone string `yaml:"timezone"`
+}
+
+type ResourcesConfig struct {
+	Ldap    *Ldap    `yaml:"ldap"`
+	Network *Network `yaml:"network"`
+	Ntp     *Ntp     `yaml:"ntp"`
+	Syslog  *Syslog  `yaml:"syslog"`
+	User    []*User  `yaml:"user'`
+}
