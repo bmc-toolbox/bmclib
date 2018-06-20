f6c67dedd23325376741e4a8388f4528c158955e
Joel Rebello <joel.rebello@booking.com>
2018-06-20 14:55:17 +0200

    setupPost: add post setup method

---
+++ butler/setupPost.go
@@ -0,0 +1,44 @@
+package butler
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/bmc-toolbox/bmcbutler/asset"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"/jrebello/go-serverdb/serverdb"
+)
+
+//Actions to be taken once a chassis was setup successfully.
+func (s *SetupAction) Post(asset *asset.Asset) {
+	log := s.Log
+	component := "Post setup"
+
+	extra := asset.Extra
+	serverId, _ := strconv.Atoi(extra["serverDbId"])
+
+	apiUser := viper.GetString("inventory.setup.serverdb.apiUser")
+	apiKey := viper.GetString("inventory.setup.serverdb.apiKey")
+
+	sdb := serverdb.New(apiUser, apiKey)
+	resp, err := sdb.UpdateChassisStatus(serverId, "installed")
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"component":         component,
+			"butler-id":         s.Id,
+			"Asset":             fmt.Sprintf("%+v", asset),
+			"Error":             err,
+			"ServerDb response": resp,
+		}).Warn("Unable to update chassis status to installed in ServerDb.")
+	}
+
+	log.WithFields(logrus.Fields{
+		"component":         component,
+		"butler-id":         s.Id,
+		"Asset":             fmt.Sprintf("%+v", s.Asset),
+		"ServerDb response": resp,
+	}).Info("Updated asset state to installed in serverDb")
+
+	return
+}
