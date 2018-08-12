86999e69a2a568b749e000349f7a47e60337d91b
Joel Rebello <joel.rebello@booking.com>
2018-08-12 16:20:52 +0200

    Fix config not found error message to include viper config file in use.

--- cmd/root.go
+++ cmd/root.go
@@ -172,9 +172,9 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Error reading config: ", err)
+		fmt.Println("Error reading config:", viper.ConfigFileUsed())
+		fmt.Println("  ->", err)
 		os.Exit(1)
 	}
 
-	//fmt.Println("Config: ", cfgFile)
 }
