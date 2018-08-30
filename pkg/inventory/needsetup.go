8de4d1380be4fb5e22a05837be6df915e85904b3
Joel Rebello <joel.rebello@booking.com>
2018-08-30 14:05:22 +0200

    needsetup: update sample code.

--- pkg/inventory/needsetup.go
+++ pkg/inventory/needsetup.go
@@ -4,13 +4,33 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/bmc-toolbox/bmcbutler/pkg/asset"
+	"github.com/bmc-toolbox/bmcbutler/pkg/config"
 )
 
 // A inventory source is required to have a type with these fields
 type NeedSetup struct {
-	Log       *logrus.Logger
-	BatchSize int                  //number of inventory assets to return per iteration
-	Channel   chan<- []asset.Asset //the channel to send inventory assets over
+	Log             *logrus.Logger
+	BatchSize       int                  //number of inventory assets to return per iteration
+	Channel         chan<- []asset.Asset //the channel to send inventory assets over
+	Config          *config.Params       //bmcbutler config
+	FilterAssetType []string
+}
+
+//AssetRetrieve looks at n.Config.FilterParams
+//and returns the appropriate function that will retrieve assets.
+func (n *NeedSetup) AssetRetrieve() func() {
+
+	//needsetup is only supported for chassis actions.
+	n.FilterAssetType = append(n.FilterAssetType, "chassis")
+
+	//Based on the filter param given, return the asset iterator method.
+	switch {
+	case n.Config.FilterParams.Serial != "":
+		return n.AssetIterBySerial
+	default:
+		return n.AssetIter
+	}
+
 }
 
 // A routine that returns data to iter over
@@ -23,7 +43,8 @@ func (n *NeedSetup) AssetIter() {
 	close(n.Channel)
 }
 
-func (n *NeedSetup) AssetIterBySerial(serial string, assetType string) {
+func (n *NeedSetup) AssetIterBySerial() {
+
 	n.Log.Println("This needs to be implemented.")
 	close(n.Channel)
 }
