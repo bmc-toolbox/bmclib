7a9775d712db7f7741b56f5cd6aee42d035be6a7
Joel Rebello <joel.rebello@booking.com>
2018-09-14 16:14:17 +0200

    idrac8: fixes #54 improve up user account management

--- providers/dell/idrac8/model.go
+++ providers/dell/idrac8/model.go
@@ -1,8 +1,12 @@
 package idrac8
 
-//https://10.193.251.5/sysmgmt/2012/server/configgroup/iDRAC.SysLog
-//{"iDRAC.SysLog":{"Port":"514","SysLogEnable":"Enabled","Server1":"provision.anycast.example.com","Server2":"","Server3":""}}
+import (
+	"encoding/xml"
+)
 
+type UserInfo map[int]User
+
+//https://10.193.251.5/sysmgmt/2012/server/configgroup/iDRAC.SysLog
 type Syslog struct {
 	Port    string `json:"Port"`
 	Enable  string `json:"SysLogEnable"`
@@ -12,8 +16,6 @@ type Syslog struct {
 }
 
 //https://10.193.251.5/sysmgmt/2012/server/configgroup/iDRAC.Users.3
-//{"iDRAC.Users":{"UserName":"@066@06f@06f@062@061@072", "Password":"@066@06f@06f@062@061@072", "Enable":"Enabled", "Privilege":"511",
-// "IpmiLanPrivilege":"Administrator", "SolEnable":"Enabled"}}
 type User struct {
 	UserName         string `json:"UserName"`
 	Password         string `json:"Password"`
@@ -23,19 +25,7 @@ type User struct {
 	SolEnable        string `json:"SolEnable"`
 }
 
-//blink identifier led - with a 1 day timeout.
-//https://10.193.251.10/data?set=IdentifyEnable:1,IdentifyTimeout:8640000
-
 //GET - params as query string
-//power cap
-//https://10.193.251.10/data?set=pbtEnabled:0,
-
-//GET - params as query string
-//timezone
-//https://10.193.251.10/data?set=tm_tz_str_zone:CET
-
-//GET - params as query string
-//ntp servers
 //https://10.193.251.10/data?set=tm_ntp_int_opmode:1,tm_ntp_str_server1:ntp0.lhr4.example.com,tm_ntp_str_server2:ntp0.ams4.example.com,tm_ntp_str_server3:ntp0.fra4.example.com
 type NtpServer struct {
 	Enable  bool   `url:"tm_ntp_int_opmode,int"`
@@ -44,21 +34,21 @@ type NtpServer struct {
 	Server3 string `url:"tm_ntp_str_server3"`
 }
 
-// LDAP settings
-// Notes: - all non-numeric,alphabetic characters are escaped
-//        - the idrac posts each payload twice?
-//        - requests can be either POST or GET except for the final one - postset?ldapconf
-//POST/GET
-//Setup ldap server
-//https://10.193.251.10/data?set=xGLServer:ldaps.example.com
-//Set object class
-// set=xGLSearchFilter:objectClass\=posixAccount
-//Set ldap groups
-// set=xGLGroup1Name:cn\=bmcAdmins\,ou\=Group\,dc\=activehotels\,dc\=com
-// set=xGLGroup2Name:cn\=bmcUsers\,ou\=Group\,dc\=activehotels\,dc\=com
+type XmlRoot struct {
+	XMLName        xml.Name         `xml:"root"`
+	Text           string           `xml:",chardata"`
+	XmlUserAccount []XmlUserAccount `xml:"user"`
+	Status         string           `xml:"status"`
+}
 
-//POST
-//Setup ldap group privileges and some other params
-//Content-Type: application/x-www-form-urlencoded
-//https://10.193.251.10/postset?ldapconf
-//data=LDAPEnableMode:3,xGLNameSearchEnabled:0,xGLBaseDN:ou%5C%3DPeople%5C%2Cdc%5C%3Dactivehotels%5C%2Cdc%5C%3Dcom,xGLUserLogin:uid,xGLGroupMem:memberUid,xGLBindDN:,xGLCertValidationEnabled:1,xGLGroup1Priv:511,xGLGroup2Priv:97,xGLGroup3Priv:0,xGLGroup4Priv:0,xGLGroup5Priv:0,xGLServerPort:636
+type XmlUserAccount struct {
+	Name          string `xml:"name"`
+	Id            int    `xml:"id"`
+	Privileges    int    `xml:"privileges"` //511 = Administrator, 0 = None,
+	Enabled       int    `xml:"enabled"`
+	LanPriv       int    `xml:"lanPriv"`    //4 = Administrator, 3 = Operator, 2 = User, 15 = None
+	SerialPriv    int    `xml:"serialPriv"` //4 = Administrator, 3 = Operator, 2 = User, 15 = None
+	SolEnabled    int    `xml:"solEnabled"`
+	SnmpV3Enabled int    `xml:"SNMPV3Enabled"`
+	SnmpPrivType  int    `xml:"snmpPrivType"`
+}
