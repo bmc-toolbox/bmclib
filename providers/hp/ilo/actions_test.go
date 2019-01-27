71f6eeab7da94d6ee95ec201b604b514d4c8c6b2
Juliano Martinez <juliano@martinez.io>
2019-01-26 22:30:48 -0800

    use random ports for the ssh daemons, it seems that it's not fast enough to shutdown in travis ci and it make tests fail

--- providers/hp/ilo/actions_test.go
+++ providers/hp/ilo/actions_test.go
@@ -5,6 +5,10 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"time"
+
+	mrand "math/rand"
+
 	"fmt"
 	"log"
 	"net"
@@ -15,6 +19,14 @@ import (
 
 // Test server based on:
 // http://grokbase.com/t/gg/golang-nuts/165yek1eje/go-nuts-creating-an-ssh-server-instance-for-tests
+
+func init() {
+	mrand.Seed(time.Now().Unix())
+}
+
+func sshServerAddress(min, max int) string {
+	return fmt.Sprintf("127.0.0.1:%d", mrand.Intn(max-min)+min)
+}
 
 var (
 	sshServer  net.Listener
@@ -50,11 +62,11 @@ func encodePrivateKeyToPEM(pk *rsa.PrivateKey) (payload []byte) {
 	return pem.EncodeToMemory(&block)
 }
 
-func runSSHServer(config *ssh.ServerConfig, loading chan interface{}) {
+func runSSHServer(config *ssh.ServerConfig, address string, loading chan interface{}) {
 	var err error
-	sshServer, err = net.Listen("tcp", "127.0.0.1:2200")
+	sshServer, err = net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("Failed to listen on 2200 (%s)", err)
+		log.Fatalf("Failed to listen on %s (%s)", address, err)
 	}
 
 	close(loading)
@@ -153,8 +165,9 @@ func setupSSH() (bmc *Ilo, err error) {
 
 	config.AddHostKey(private)
 
+	address := sshServerAddress(2000, 4000)
 	loading := make(chan interface{})
-	go runSSHServer(config, loading)
+	go runSSHServer(config, address, loading)
 	<-loading
 
 	bmc, err = setup()
