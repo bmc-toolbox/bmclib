bmc-toolbox/bmclib

commit 628a36a4e674f3733409e71baa0fa11913a4966a

idrac9: default GroupAttributeIsDN to false, and use the full DN for role groups

providers/dell/idrac9/configure.go
@@ -254,7 +254,7 @@ func (i *IDrac9) Ldap(cfg *cfgresources.Ldap) (err error) {
 		"Port":                 "636",
 		"UserAttribute":        "uid",
 		"GroupAttribute":       "memberUid",
-		"GroupAttributeIsDN":   "Enabled",
+		"GroupAttributeIsDN":   "Disabled",
 		"CertValidationEnable": "Disabled",
 		"SearchFilter":         "objectClass=posixAccount",
 	}
@@ -366,7 +366,7 @@ func (i *IDrac9) LdapGroup(cfg []*cfgresources.LdapGroup, cfgLdap *cfgresources.
 				}
 			}
 
-			role.DN = cfgRole.Group
+			role.DN = fmt.Sprintf("%s,%s", cfgRole.Group, cfgRole.GroupBaseDn)
 
 			//set appropriate privileges
 			if cfgRole.Role == "admin" {

commit e87cfd10ffa52308d2286874dcd730cb81834828

idrac9: fix role group exists validation issue introduced in 628a36a4e6

providers/dell/idrac9/configure.go
@@ -345,7 +345,13 @@ func (i *IDrac9) LdapGroup(cfg []*cfgresources.LdapGroup, cfgLdap *cfgresources.
 
 	//for each configuration ldap role group
 	for _, cfgRole := range cfg {
-		roleID, role, rExists := ldapRoleGroupInIdrac(cfgRole.Group, idracLdapRoleGroups)
+
+		// the distinguished name of the group
+		// example, if the GroupBaseDn is ou=Group,dc=example,dc=com and the Group is cn=fooUsers
+		// the groupDN will be Group+GroupBaseDn = cn=fooUsers,ou=Group,dc=example,dc=com
+		var groupDN = fmt.Sprintf("%s,%s", cfgRole.Group, cfgRole.GroupBaseDn)
+
+		roleID, role, rExists := ldapRoleGroupInIdrac(groupDN, idracLdapRoleGroups)
 
 		//role to be added/updated
 		if cfgRole.Enable {
@@ -366,7 +372,7 @@ func (i *IDrac9) LdapGroup(cfg []*cfgresources.LdapGroup, cfgLdap *cfgresources.
 				}
 			}
 
-			role.DN = fmt.Sprintf("%s,%s", cfgRole.Group, cfgRole.GroupBaseDn)
+			role.DN = groupDN
 
 			//set appropriate privileges
 			if cfgRole.Role == "admin" {
e87cfd10ffa52308d2286874dcd730cb81834828
Joel Rebello <joel.rebello@booking.com>
2019-05-07 11:49:08 +0200

    idrac9: fix role group exists validation issue introduced in 628a36a4e6

--- providers/dell/idrac9/configure.go
+++ providers/dell/idrac9/configure.go
@@ -345,7 +345,13 @@ func (i *IDrac9) LdapGroup(cfg []*cfgresources.LdapGroup, cfgLdap *cfgresources.
 
 	//for each configuration ldap role group
 	for _, cfgRole := range cfg {
-		roleID, role, rExists := ldapRoleGroupInIdrac(cfgRole.Group, idracLdapRoleGroups)
+
+		// the distinguished name of the group
+		// example, if the GroupBaseDn is ou=Group,dc=example,dc=com and the Group is cn=fooUsers
+		// the groupDN will be Group+GroupBaseDn = cn=fooUsers,ou=Group,dc=example,dc=com
+		var groupDN = fmt.Sprintf("%s,%s", cfgRole.Group, cfgRole.GroupBaseDn)
+
+		roleID, role, rExists := ldapRoleGroupInIdrac(groupDN, idracLdapRoleGroups)
 
 		//role to be added/updated
 		if cfgRole.Enable {
@@ -366,7 +372,7 @@ func (i *IDrac9) LdapGroup(cfg []*cfgresources.LdapGroup, cfgLdap *cfgresources.
 				}
 			}
 
-			role.DN = fmt.Sprintf("%s,%s", cfgRole.Group, cfgRole.GroupBaseDn)
+			role.DN = groupDN
 
 			//set appropriate privileges
 			if cfgRole.Role == "admin" {
