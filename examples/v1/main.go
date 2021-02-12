b779ec1e1775976c6c3ef4e0d25b92d061d5782e
Joel Rebello <jrebello@packet.com>
2021-02-12 16:45:01 +0100

    Add v1, v2 bmclib client examples

---
+++ examples/v1/main.go
@@ -0,0 +1,59 @@
+package main
+
+// This snippet utilizes the 'v1' older bmclib interface methods
+// it connects to the bmc and retries its version
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/bmc-toolbox/bmclib/devices"
+	"github.com/bmc-toolbox/bmclib/discover"
+	"github.com/bombsimon/logrusr"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx := context.TODO()
+	//defer cancel()
+	host := ""
+	user := ""
+	pass := ""
+
+	l := logrus.New()
+	l.Level = logrus.TraceLevel
+	logger := logrusr.NewLogger(l)
+
+	c, err := discover.ScanAndConnect(
+		host,
+		user,
+		pass,
+		discover.WithContext(ctx),
+		discover.WithLogger(logger),
+		discover.WithProbeHint(discover.ProbeASRockRack),
+	)
+
+	if err != nil {
+		logger.Error(err, "Error connecting to bmc")
+	}
+
+	bmc := c.(devices.Bmc)
+
+	err = bmc.CheckCredentials()
+	if err != nil {
+		logger.Error(err, "Failed to validate credentials")
+		os.Exit(1)
+	}
+
+	defer bmc.Close(ctx)
+
+	v, err := bmc.GetBMCVersion(ctx)
+	if err != nil {
+		logger.Error(err, "Error getting bmc version")
+		os.Exit(1)
+	}
+	fmt.Println(v)
+
+}
---
+++ examples/v2/main.go
@@ -0,0 +1,82 @@
+package main
+
+/*
+ This utilizes the 'v2' bmclib interface methods
+*/
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/bmc-toolbox/bmclib"
+	"github.com/bmc-toolbox/bmclib/logging"
+	"github.com/bmc-toolbox/bmclib/providers/asrockrack"
+	"github.com/bombsimon/logrusr"
+	"github.com/jacobweinstock/registrar"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Hour)
+	defer cancel()
+	host := ""
+	port := ""
+	user := ""
+	pass := ""
+
+	log := logging.DefaultLogger()
+
+	asrockRack, err := asrockrack.New(ctx, host, user, pass, log)
+	if err != nil {
+		log.Error(err, "failed to init asrockrack instance")
+	}
+
+	// how do I get logr to log at trace/debug level
+	l := logrus.New()
+	l.Level = logrus.InfoLevel
+	logg := logrusr.NewLogger(l)
+
+	// define and register driver
+	drivers := registrar.NewRegistry()
+
+	// The driver interface being provided could implement methods for ProviderName, ProviderProtocol etc
+	// that way we don't have to pass all of these in individually?
+	drivers.Register(asrockrack.ProviderName, asrockrack.ProviderProtocol, asrockrack.Features, nil, asrockRack)
+	regOptions := bmclib.WithRegistry(drivers)
+
+	// bmclib client - setting the logger as a param doesn't work - its overwritten by the default logger
+	cl := bmclib.NewClient(host, port, user, pass, regOptions)
+	cl.Registry.Logger = logg
+
+	// open connection
+	err = cl.Open(ctx)
+	if err != nil {
+		log.Error(err, "bmc login failed")
+	}
+
+	defer cl.Close(ctx)
+
+	v, err := cl.GetBMCVersion(ctx)
+	if err != nil {
+		log.Error(err, "unable to retrieve BMC version")
+	}
+
+	fmt.Println("BMC version: " + v)
+	//	err = cl.UpdateBMCFirmware(ctx, "/tmp/E3C246D4I-NL_L0.03.00.ima")
+	//	if err != nil {
+	//		log.Error(err, "error updating BMC firmware")
+	//	}
+
+	//	v, err = cl.GetBIOSVersion(ctx)
+	//	if err != nil {
+	//		log.Error(err, "unable to retrieve BIOS version")
+	//	}
+	//
+	//	fmt.Println("BIOS version: " + v)
+	//
+	//	err = cl.UpdateBIOSFirmware(ctx, "/tmp/E6D4INL2.07B")
+	//	if err != nil {
+	//		log.Error(err, "error updating BIOS firmware")
+	//	}
+}
