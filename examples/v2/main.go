a09e6614a858613ecfec22ac8c55a6f51c7fbd9f
Joel Rebello <jrebello@packet.com>
2021-02-16 20:16:38 +0100

    examples/v2: clean up since the provider driver is registered

--- examples/v2/main.go
+++ examples/v2/main.go
@@ -7,76 +7,39 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/bmc-toolbox/bmclib"
-	"github.com/bmc-toolbox/bmclib/logging"
-	"github.com/bmc-toolbox/bmclib/providers/asrockrack"
 	"github.com/bombsimon/logrusr"
-	"github.com/jacobweinstock/registrar"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Hour)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
-	host := ""
+	host := "127.0.0.1"
 	port := ""
-	user := ""
-	pass := ""
+	user := "foo"
+	pass := "bar"
 
-	log := logging.DefaultLogger()
-
-	asrockRack, err := asrockrack.New(ctx, host, user, pass, log)
-	if err != nil {
-		log.Error(err, "failed to init asrockrack instance")
-	}
-
-	// how do I get logr to log at trace/debug level
+	cl := bmclib.NewClient(host, port, user, pass)
 	l := logrus.New()
-	l.Level = logrus.InfoLevel
-	logg := logrusr.NewLogger(l)
+	l.Level = logrus.TraceLevel
+	cl.Logger = logrusr.NewLogger(l)
 
-	// define and register driver
-	drivers := registrar.NewRegistry()
-
-	// The driver interface being provided could implement methods for ProviderName, ProviderProtocol etc
-	// that way we don't have to pass all of these in individually?
-
-	regOptions := bmclib.WithRegistry(drivers)
-
-	// bmclib client - setting the logger as a param doesn't work - its overwritten by the default logger
-	cl := bmclib.NewClient(host, port, user, pass, regOptions)
-	cl.Registry.Logger = logg
-
-	// open connection
-	err = cl.Open(ctx)
+	err := cl.Open(ctx)
 	if err != nil {
-		log.Error(err, "bmc login failed")
+		log.Fatal(err, "bmc login failed")
 	}
 
 	defer cl.Close(ctx)
 
 	v, err := cl.GetBMCVersion(ctx)
 	if err != nil {
-		log.Error(err, "unable to retrieve BMC version")
+		log.Fatal(err, "unable to retrieve BMC version")
 	}
 
 	fmt.Println("BMC version: " + v)
-	//	err = cl.UpdateBMCFirmware(ctx, "/tmp/E3C246D4I-NL_L0.03.00.ima")
-	//	if err != nil {
-	//		log.Error(err, "error updating BMC firmware")
-	//	}
 
-	//	v, err = cl.GetBIOSVersion(ctx)
-	//	if err != nil {
-	//		log.Error(err, "unable to retrieve BIOS version")
-	//	}
-	//
-	//	fmt.Println("BIOS version: " + v)
-	//
-	//	err = cl.UpdateBIOSFirmware(ctx, "/tmp/E6D4INL2.07B")
-	//	if err != nil {
-	//		log.Error(err, "error updating BIOS firmware")
-	//	}
 }
