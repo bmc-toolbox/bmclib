319684a1316a52f243d8aa9cd7fb68f1591fc5e0
Jacob Weinstock <jakobweinstock@gmail.com>
2022-02-01 12:06:04 -0700

    Remove bou.ke/monkey:
    The license for bou.ke/monkey prohibits use
    of the library for any reason.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- providers/ipmitool/ipmitool_test.go
+++ providers/ipmitool/ipmitool_test.go
@@ -2,17 +2,13 @@ package ipmitool
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"reflect"
 	"testing"
-	"time"
 
-	"bou.ke/monkey"
 	"github.com/bmc-toolbox/bmclib/internal/ipmi"
 	"github.com/bmc-toolbox/bmclib/logging"
 )
@@ -39,47 +35,6 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 	os.RemoveAll(tempDir)
 	os.Exit(code)
-}
-
-func TestIsCompatible(t *testing.T) {
-	testCases := []struct {
-		name string
-		ok   bool
-	}{
-		{"true", true},
-		{"false", false},
-	}
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			var ipm *ipmi.Ipmi
-			monkey.PatchInstanceMethod(reflect.TypeOf(ipm), "PowerState", func(_ *ipmi.Ipmi, _ context.Context) (status string, err error) {
-				if !tc.ok {
-					err = errors.New("not compatible")
-				}
-				return "on", err
-			})
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-			defer cancel()
-			user := "ADMIN"
-			pass := "ADMIN"
-			host := "127.1.1.1"
-			port := "623"
-			i, _ := ipmi.New(user, pass, host+":"+port)
-			c := Conn{
-				Host: host,
-				Port: port,
-				User: user,
-				Pass: pass,
-				Log:  logging.DefaultLogger(),
-				con:  i,
-			}
-			ok := c.Compatible(ctx)
-			if ok != tc.ok {
-				t.Fatalf("got: %v, expected: %v", ok, tc.ok)
-			}
-		})
-	}
 }
 
 func TestPowerState(t *testing.T) {
