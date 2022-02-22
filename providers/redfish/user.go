4a6f31e756164e7e36c65940b357c0fd0eb4aab3
Joel Rebello <jrebello@packet.com>
2022-02-22 15:13:18 +0100

    providers/redfish: implement user deletion method

--- providers/redfish/user.go
+++ providers/redfish/user.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	ErrNoUserSlotsAvailable = errors.New("no user account slots available")
-	ErrUserNotPresent       = errors.New("given user not present")
+	ErrUserNotPresent       = errors.New("account with username was not found")
 	ErrUserPassParams       = errors.New("user and pass parameters required")
 	ErrUserExists           = errors.New("user exists")
 	ErrInvalidUserRole      = errors.New("invalid user role")
@@ -132,4 +132,46 @@ func (c *Conn) UserCreate(ctx context.Context, user, pass, role string) (ok bool
 	}
 
 	return false, ErrNoUserSlotsAvailable
+}
+
+// UserDelete deletes a user account
+func (c *Conn) UserDelete(ctx context.Context, user string) (ok bool, err error) {
+	if user == "" {
+		return false, ErrUserPassParams
+	}
+
+	service, err := c.conn.Service.AccountService()
+	if err != nil {
+		return false, err
+	}
+
+	// fetch current list of accounts
+	accounts, err := service.Accounts()
+	if err != nil {
+		return false, err
+	}
+
+	// identify account slot
+	for _, account := range accounts {
+		// Dell iDracs don't want us to create/delete accounts in these slots
+		if internal.StringInSlice(account.ID, []string{"1"}) {
+			continue
+		}
+
+		account := account
+		if account.UserName == user {
+			account.Enabled = false
+			account.UserName = ""
+			account.Password = ""
+
+			err := account.Update()
+			if err != nil {
+				return false, err
+			}
+
+			return true, nil
+		}
+	}
+
+	return false, ErrUserNotPresent
 }
