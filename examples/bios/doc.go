209e24ab49fd453f66b0f2eacc9a63724914c149
Diogo Mendes Matsubara <diogo.mendesmatsubara@eu.equinix.com>
2023-03-08 16:30:37 +0100

    Add example for GetBiosConfiguration

---
+++ examples/bios/doc.go
@@ -0,0 +1,6 @@
+/*
+bios is an example commmand that retrieves BIOS configuration information and prints it out
+
+	$ BMC_HOST=1.2.3.4 BMC_USERNAME=foo BMC_PASSWORD=bar go run ./examples/bios/main.go
+*/
+package main
