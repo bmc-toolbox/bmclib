621d498c7329996f7f5cad04035e2aff681a20d7
Diogo Mendes Matsubara <diogo.mendesmatsubara@eu.equinix.com>
2023-03-08 16:06:24 +0100

    Use redfishwrapper instead of underlying client
     - Use the redfishwrapper instead of exporting the underlying redfish client,
     since the wrapper ensures an active session

--- providers/redfish/bios.go
+++ providers/redfish/bios.go
@@ -2,20 +2,28 @@ package redfish
 
 import (
 	"context"
-
-	gofishrf "github.com/stmcginnis/gofish/redfish"
 )
 
 func (c *Conn) GetBiosConfiguration(ctx context.Context) (biosConfig map[string]string, err error) {
-	// TODO: replace uri parameter with configurable value
-	b, err := gofishrf.GetBios(c.redfishwrapper.Client, "/redfish/v1/Systems/System.Embedded.1/Bios")
+	systems, err := c.redfishwrapper.Systems()
 	if err != nil {
 		return nil, err
 	}
 
 	biosConfig = make(map[string]string)
-	for k := range b.Attributes {
-		biosConfig[k] = b.Attributes.String(k)
+	for _, sys := range systems {
+		if !compatibleOdataID(sys.ODataID, systemsOdataIDs) {
+			continue
+		}
+
+		bios, err := sys.Bios()
+		if err != nil {
+			return nil, err
+		}
+
+		for attr := range bios.Attributes {
+			biosConfig[attr] = bios.Attributes.String(attr)
+		}
 	}
 	return biosConfig, nil
 }
