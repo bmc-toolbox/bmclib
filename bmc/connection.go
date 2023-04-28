60a5bc890fbc5e921bfc597e49312a662eaf5222
Chris Doherty <chris.doherty4@gmail.com>
2023-04-27 21:33:42 -0500

    Refactor BMC provider opener to probe concurrently
    When determining what interfaces are supported by a BMC we probe
    serially. This change probes interfaces concurrently to try and give all
    probes the maximum timeout.

--- bmc/connection.go
+++ bmc/connection.go
@@ -3,6 +3,7 @@ package bmc
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/hashicorp/go-multierror"
@@ -28,67 +29,105 @@ type connectionProviders struct {
 // OpenConnectionFromInterfaces will try all opener interfaces and remove failed ones.
 // The reason failed ones need to be removed is so that when other methods are called (like powerstate)
 // implementations that have connections wont nil pointer error when their connection fails.
-func OpenConnectionFromInterfaces(ctx context.Context, timeout time.Duration, generic []interface{}) (opened []interface{}, metadata Metadata, err error) {
-	var metadataLocal Metadata
+func OpenConnectionFromInterfaces(ctx context.Context, timeout time.Duration, providers []interface{}) (opened []interface{}, _ Metadata, err error) {
+	var metadata Metadata
 
-	for _, elem := range generic {
-		// return immediately if the context is done/terminated/etc
-		select {
-		case <-ctx.Done():
-			err = multierror.Append(err, ctx.Err())
-			return nil, metadata, err
-		default:
-		}
-		// get the provider name for use in metadata
-		providerName := getProviderName(elem)
-		// now, try to open connections
+	// Return immediately if the context is done.
+	select {
+	case <-ctx.Done():
+		return nil, metadata, multierror.Append(err, ctx.Err())
+	default:
+	}
+
+	// Create a context with the specified timeout. This is done for backward compatibility but
+	// we should consider removing the timeout parameter alltogether given the context will
+	// container the timeout.
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	// result facilitates communication of data between the concurrent opener goroutines and
+	// the the parent goroutine.
+	type result struct {
+		ProviderName string
+		Opener       Opener
+		Err          error
+	}
+
+	// Create a channel to communicate results between opener goroutines and the parent goroutine.
+	results := make(chan result)
+
+	// Use a WaitGroup to control closing of the results channel when all opener goroutines finish.
+	var wg sync.WaitGroup
+
+	// For every provider, launch a goroutine that attempts to open a connection and report
+	// back via the results channel what happened.
+	for _, elem := range providers {
 		switch p := elem.(type) {
 		case Opener:
-			metadataLocal.ProvidersAttempted = append(metadataLocal.ProvidersAttempted, providerName)
-			tmpctx, cancel := context.WithTimeout(ctx, timeout)
-			defer cancel()
-			er := p.Open(tmpctx)
-			if er != nil {
-				err = multierror.Append(err, errors.WithMessagef(er, "provider: %v", providerName))
-				continue
-			}
+			providerName := getProviderName(elem)
+			metadata.ProvidersAttempted = append(metadata.ProvidersAttempted, providerName)
 
-			opened = append(opened, elem)
-			metadataLocal.SuccessfulOpenConns = append(metadataLocal.SuccessfulOpenConns, providerName)
+			wg.Add(1)
+			go func(provider Opener, providerName string) {
+				defer wg.Done()
+				res := result{ProviderName: providerName, Opener: provider}
+
+				if err := provider.Open(ctx); err != nil {
+					res.Err = errors.WithMessagef(err, "provider: %v", providerName)
+				}
+
+				results <- res
+			}(p, providerName)
 
 		default:
-			e := fmt.Sprintf("not a Opener implementation: %T", p)
-			err = multierror.Append(err, errors.New(e))
+			err = multierror.Append(err, fmt.Errorf("not a Opener implementation: %T", p))
 		}
 	}
-	if len(opened) == 0 {
-		return nil, metadataLocal, multierror.Append(err, errors.New("no Opener implementations found"))
+
+	// Launch the goroutine to close the results channel ensuring we can exit the for-range over
+	// the results channel below.
+	go func() { wg.Wait(); close(results) }()
+
+	// Gather and handle results from the opener goroutines.
+	for res := range results {
+		if res.Err != nil {
+			err = multierror.Append(err, res.Err)
+			continue
+		}
+
+		opened = append(opened, res.Opener)
+		metadata.SuccessfulOpenConns = append(metadata.SuccessfulOpenConns, res.ProviderName)
 	}
-	return opened, metadataLocal, nil
+
+	if len(opened) == 0 {
+		return nil, metadata, multierror.Append(err, errors.New("no Opener implementations found"))
+	}
+
+	return opened, metadata, nil
 }
 
 // closeConnection closes a connection to a BMC, trying all interface implementations passed in
-func closeConnection(ctx context.Context, c []connectionProviders) (metadata Metadata, err error) {
-	var metadataLocal Metadata
+func closeConnection(ctx context.Context, c []connectionProviders) (_ Metadata, err error) {
+	var metadata Metadata
 	var connClosed bool
 
 	for _, elem := range c {
 		if elem.closer == nil {
 			continue
 		}
-		metadataLocal.ProvidersAttempted = append(metadataLocal.ProvidersAttempted, elem.name)
+		metadata.ProvidersAttempted = append(metadata.ProvidersAttempted, elem.name)
 		closeErr := elem.closer.Close(ctx)
 		if closeErr != nil {
 			err = multierror.Append(err, errors.WithMessagef(closeErr, "provider: %v", elem.name))
 			continue
 		}
 		connClosed = true
-		metadataLocal.SuccessfulCloseConns = append(metadataLocal.SuccessfulCloseConns, elem.name)
+		metadata.SuccessfulCloseConns = append(metadata.SuccessfulCloseConns, elem.name)
 	}
 	if connClosed {
-		return metadataLocal, nil
+		return metadata, nil
 	}
-	return metadataLocal, multierror.Append(err, errors.New("failed to close connection"))
+	return metadata, multierror.Append(err, errors.New("failed to close connection"))
 }
 
 // CloseConnectionFromInterfaces identifies implementations of the Closer() interface and and passes the found implementations to the closeConnection() wrapper
