6cc4d21d25688eabcfca416b1d5aa5f9a2835460
Joel Rebello <jrebello@packet.com>
2023-05-25 11:20:00 +0200

    providers/dell: update to check manufacturer on Open()
    This is to ensure this provider proceeds only if its running on a Dell system.

--- providers/dell/idrac_test.go
+++ providers/dell/idrac_test.go
@@ -20,54 +20,106 @@ const (
 	fixturesDir = "./fixtures"
 )
 
-func serviceRoot(w http.ResponseWriter, r *http.Request) {
-	// expect either GET or Delete methods
-	if r.Method != http.MethodGet && r.Method != http.MethodDelete {
-		w.WriteHeader(http.StatusNotFound)
-	}
-	fixture := fixturesDir + "/serviceroot.json"
-	fh, err := os.Open(fixture)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer fh.Close()
-
-	b, err := io.ReadAll(fh)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, _ = w.Write(b)
-}
-
 func Test_Screenshot(t *testing.T) {
 	// byte slice instead of a real image
 	img := []byte(`foobar`)
 
+	// endpoint to handler funcs
+	type handlerFuncMap map[string]func(http.ResponseWriter, *http.Request)
+
 	testcases := []struct {
-		name     string
-		imgbytes []byte
-		handler  func(http.ResponseWriter, *http.Request)
+		name           string
+		imgbytes       []byte
+		handlerFuncMap handlerFuncMap
 	}{
 		{
 			"happy path",
 			[]byte(`foobar`),
-			func(w http.ResponseWriter, r *http.Request) {
-				assert.Equal(t, r.Method, http.MethodPost)
+			handlerFuncMap{
+				// service root
+				"/redfish/v1/": func(w http.ResponseWriter, r *http.Request) {
+					// expect either GET or Delete methods
+					if r.Method != http.MethodGet && r.Method != http.MethodDelete {
+						w.WriteHeader(http.StatusNotFound)
+					}
 
-				assert.Equal(t, r.Header.Get("Content-Type"), "application/json")
+					fixture := fixturesDir + "/serviceroot.json"
+					fh, err := os.Open(fixture)
+					if err != nil {
+						log.Fatal(err)
+					}
 
-				b, err := io.ReadAll(r.Body)
-				if err != nil {
-					t.Fatal(err)
-				}
-				assert.Equal(t, []byte(`{"FileType":"ServerScreenShot"}`), b)
+					defer fh.Close()
 
-				encoded := base64.RawStdEncoding.EncodeToString(img)
-				respFmtStr := `{"@Message.ExtendedInfo":[{"Message":"Successfully Completed Request","MessageArgs":[],"MessageArgs@odata.count":0,"MessageId":"Base.1.8.Success","RelatedProperties":[],"RelatedProperties@odata.count":0,"Resolution":"None","Severity":"OK"},{"Message":"The Export Server Screen Shot operation successfully exported the server screen shot file.","MessageArgs":[],"MessageArgs@odata.count":0,"MessageId":"IDRAC.2.5.LC080","RelatedProperties":[],"RelatedProperties@odata.count":0,"Resolution":"Download the encoded Base64 format server screen shot file, decode the Base64 file and then save it as a *.png file.","Severity":"Informational"}],"ServerScreenshotFile":"%s"}`
+					b, err := io.ReadAll(fh)
+					if err != nil {
+						log.Fatal(err)
+					}
 
-				_, _ = w.Write([]byte(fmt.Sprintf(respFmtStr, encoded)))
+					_, _ = w.Write(b)
+				},
+
+				"/redfish/v1/Systems": func(w http.ResponseWriter, r *http.Request) {
+					// expect either GET or Delete methods
+					if r.Method != http.MethodGet && r.Method != http.MethodDelete {
+						w.WriteHeader(http.StatusNotFound)
+					}
+
+					fixture := fixturesDir + "/systems.json"
+					fh, err := os.Open(fixture)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					defer fh.Close()
+
+					b, err := io.ReadAll(fh)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					_, _ = w.Write(b)
+				},
+
+				"/redfish/v1/Systems/System.Embedded.1": func(w http.ResponseWriter, r *http.Request) {
+					// expect either GET or Delete methods
+					if r.Method != http.MethodGet && r.Method != http.MethodDelete {
+						w.WriteHeader(http.StatusNotFound)
+					}
+
+					fixture := fixturesDir + "/systems_embedded.1.json"
+					fh, err := os.Open(fixture)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					defer fh.Close()
+
+					b, err := io.ReadAll(fh)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					_, _ = w.Write(b)
+				},
+
+				// screenshot endpoint
+				redfishV1Prefix + screenshotEndpoint: func(w http.ResponseWriter, r *http.Request) {
+					assert.Equal(t, r.Method, http.MethodPost)
+
+					assert.Equal(t, r.Header.Get("Content-Type"), "application/json")
+
+					b, err := io.ReadAll(r.Body)
+					if err != nil {
+						t.Fatal(err)
+					}
+					assert.Equal(t, []byte(`{"FileType":"ServerScreenShot"}`), b)
+
+					encoded := base64.RawStdEncoding.EncodeToString(img)
+					respFmtStr := `{"@Message.ExtendedInfo":[{"Message":"Successfully Completed Request","MessageArgs":[],"MessageArgs@odata.count":0,"MessageId":"Base.1.8.Success","RelatedProperties":[],"RelatedProperties@odata.count":0,"Resolution":"None","Severity":"OK"},{"Message":"The Export Server Screen Shot operation successfully exported the server screen shot file.","MessageArgs":[],"MessageArgs@odata.count":0,"MessageId":"IDRAC.2.5.LC080","RelatedProperties":[],"RelatedProperties@odata.count":0,"Resolution":"Download the encoded Base64 format server screen shot file, decode the Base64 file and then save it as a *.png file.","Severity":"Informational"}],"ServerScreenshotFile":"%s"}`
+
+					_, _ = w.Write([]byte(fmt.Sprintf(respFmtStr, encoded)))
+				},
 			},
 		},
 	}
@@ -75,12 +127,10 @@ func Test_Screenshot(t *testing.T) {
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			mux := http.NewServeMux()
-			// default redfish handler
-			// mux.HandleFunc("/redfish/v1/SessionService/Sessions", sessionService)
-			mux.HandleFunc("/redfish/v1/", serviceRoot)
 
-			// screenshot handler
-			mux.HandleFunc(redfishV1Prefix+screenshotEndpoint, tc.handler)
+			for endpoint, handler := range tc.handlerFuncMap {
+				mux.HandleFunc(endpoint, handler)
+			}
 
 			server := httptest.NewTLSServer(mux)
 			defer server.Close()
@@ -90,8 +140,8 @@ func Test_Screenshot(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			// os.Setenv("DEBUG_BMCLIB", "true")
-			client := New(parsedURL.Hostname(), "", "", logr.Discard(), WithPort(parsedURL.Port()))
+			//os.Setenv("DEBUG_BMCLIB", "true")
+			client := New(parsedURL.Hostname(), "", "", logr.Discard(), WithPort(parsedURL.Port()), WithUseBasicAuth(true))
 
 			err = client.Open(context.TODO())
 			if err != nil {
