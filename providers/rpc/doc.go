4d9b3c738500602d5bb41b6e7d5fc66756c8a85a
Jacob Weinstock <jakobweinstock@gmail.com>
2023-09-11 14:18:54 -0600

    Add doc comment on HMAC
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- providers/rpc/doc.go
+++ providers/rpc/doc.go
@@ -4,5 +4,9 @@ It allows users a simple way to interoperate with an existing/bespoke out-of-ban
 
 The rpc provider request/response payloads are modeled after JSON-RPC 2.0, but are not JSON-RPC 2.0
 compliant so as to allow for more flexibility and interoperability with existing systems.
+
+The rpc provider has options that can be set to include an HMAC signature in the request header.
+It follows the features found at https://webhooks.fyi/security/hmac, this includes hash algorithms sha256
+and sha512, replay prevention, versioning, and key rotation.
 */
 package rpc
